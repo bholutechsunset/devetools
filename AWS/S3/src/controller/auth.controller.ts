d7c3672f973feb2c9a98616198d572c8d341f997
bholusaini <bholusaini686@gmail.com>
2026-07-18 01:22:57 +0530

    s3

---
+++ AWS/S3/src/controller/auth.controller.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from "express"
+import AuthModel from "../model/auth.model"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import mongoose from "mongoose"
+import { CatchError, TryError } from "../util/error"
+
+const accessTokenExpiry = '10m'
+
+interface PayloadInterface {
+    id: mongoose.Types.ObjectId
+    fullname: string
+    email: string
+    mobile: string
+}
+
+const generateToken = (payload: PayloadInterface)=>{
+    const accessToken = jwt.sign(payload, process.env.AUTH_SECRET!, {expiresIn: accessTokenExpiry})
+    return accessToken
+}
+
+export const signup = async (req: Request, res: Response)=>{
+    try {
+        await AuthModel.create(req.body)
+        res.json({message: "Signup success"})
+    }
+    catch(err: unknown)
+    {
+        if(err instanceof Error)
+        res.status(500).json({message: err.message})
+    }
+}
+
+export const login = async (req: Request, res: Response)=>{
+   try {
+        const {email, password} = req.body
+        const user = await AuthModel.findOne({email})
+        
+        if(!user)
+        throw TryError("User not found, please try to signup first", 404)
+        
+        const isLogin = await bcrypt.compare(password, user.password)
+
+        if(!isLogin)
+        throw TryError("Invalid credentials email or password incorrect", 401)
+
+        const payload = {
+            id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            mobile: user.mobile
+        }
+        const options = {
+            httpOnly: true,
+            maxAge: (10*60)*1000,
+            secure: false,
+            domain: 'localhost'
+        }
+        const accessToken = generateToken(payload)
+        res.cookie("accessToken", accessToken, options)
+        res.json({message: 'Login success'})
+   }
+   catch(err: unknown)
+   {
+        CatchError(err, res, "Login failed please try after sometime")
+   }
+}
+
+export const forgotPassword = (req: Request, res: Response)=>{
+    res.send("hello")
+}
+
+export const getSession = async (req: Request, res: Response)=>{
+    try {
+        const accessToken = req.cookies.accessToken
+
+        if(!accessToken)
+            throw TryError("Invalid session", 401)
+
+        const session = await jwt.verify(accessToken, process.env.AUTH_SECRET!)
+        res.json(session)
+    }
+    catch(err)
+    {
+        CatchError(err, res, "Invalid session")
+    }
+}
---
+++ AWS/S3/src/index.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import mongoose from 'mongoose'
+mongoose.connect(process.env.DB!)
+
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import AuthRouter from './router/auth.router'
+import StorageRouter from './router/storage.router'
+const app = express()
+app.listen(
+    process.env.PORT || 8080, 
+    ()=>console.log(`Server is running on ${process.env.PORT}`)
+)
+
+app.use(cors({
+    origin: process.env.CLIENT,
+    credentials: true
+}))
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use("/auth", AuthRouter)
+app.use("/storage", StorageRouter)
